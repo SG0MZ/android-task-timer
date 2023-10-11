e31ea2c499eefd4da5df40f28e95e0c3fee08eeb
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-11 15:27:26 -0600

    uriMatcher - First Commit

--- app/src/main/java/com/example/tasktimer/AppProvider.kt
+++ app/src/main/java/com/example/tasktimer/AppProvider.kt
@@ -4,12 +4,15 @@ import android.content.ContentProvider
 import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteQueryBuilder
 import android.net.Uri
 import android.util.Log
+import androidx.navigation.NavArgs
+import java.lang.IllegalArgumentException
 
 private const val TAG = "AppProvider"
 
-private val CONTENT_AUTHORITY = "learnprogramming.academy.tasktimer.provider"
+const val CONTENT_AUTHORITY = "learnprogramming.academy.tasktimer.provider"
 
 private const val TASKS = 100
 private const val TASKS_ID = 101
@@ -20,6 +23,8 @@ private const val TIMINGS_ID = 201
 private const val TASK_DURATIONS = 400
 private const val TASK_DURATIONS_ID = 401
 
+val CONTENT_AUTHORITY_URI: Uri = Uri.parse("content://$CONTENT_AUTHORITY")
+
 class AppProvider: ContentProvider() {
 
     private val uriMatcher by lazy { buildUriMatcher() }
@@ -28,21 +33,64 @@ class AppProvider: ContentProvider() {
         Log.d(TAG,"buildUriMatcher starts")
         val matcher = UriMatcher(UriMatcher.NO_MATCH)
 
+        matcher.addURI(CONTENT_AUTHORITY,TasksContract.TABLE_NAME, TASKS)
+        matcher.addURI(CONTENT_AUTHORITY,"${TasksContract.TABLE_NAME}/#", TASKS_ID)
+
         return matcher
     }
 
     override fun onCreate(): Boolean {
-        TODO("Not yet implemented")
+        Log.d(TAG,"onCreate: starts")
+        return true
     }
 
     override fun query(
-        p0: Uri,
-        p1: Array<out String>?,
-        p2: String?,
-        p3: Array<out String>?,
-        p4: String?
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        Log.d(TAG,"query: called with uri $uri")
+        val match = uriMatcher.match(uri)
+        Log.d(TAG,"query: match is $match")
+
+        val queryBuilder: SQLiteQueryBuilder = SQLiteQueryBuilder()
+
+        when(match) {
+            TASKS -> {
+                queryBuilder.tables = TasksContract.TABLE_NAME
+            }
+            TASKS_ID -> {
+                queryBuilder.tables = TasksContract.TABLE_NAME
+                val taskId = TasksContract.getId(uri)
+                queryBuilder.appendWhereEscapeString("${TasksContract.Columns.ID} = $taskId")
+            }
+
+//            TIMINGS -> queryBuilder.tables = TimingsContract.TABLE_NAME
+//
+//            TIMINGS_ID -> {
+//                queryBuilder.tables = TimingsContract.TABLE_NAME
+//                val timingId = TimingsContract.getId(uri)
+//                queryBuilder.appendWhereEscapeString("${TimingsContract.Columns.ID} = $timingId")
+//            }
+//
+//            TASK_DURATIONS -> queryBuilder.tables = DurationsContract.TABLE_NAME
+//
+//            TASK_DURATIONS_ID -> {
+//                queryBuilder.tables = DurationsContract.TABLE_NAME
+//                val durationId = DurationsContract.getId(uri)
+//                queryBuilder.appendWhereEscapeString("${DurationsContract.Columns.ID} = $durationId")
+//            }
+
+            else -> throw IllegalArgumentException("Unknown URI: $uri")
+        }
+
+        val db = AppDatabase.getInstance(context).readableDatabase
+        val cursor = queryBuilder.query(db,projection,selection,selectionArgs,null,null,sortOrder)
+        Log.d(TAG,"query: rows in returned cursos = ${cursor.count}")
+
+        return cursor
     }
 
     override fun getType(p0: Uri): String? {
