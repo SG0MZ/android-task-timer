78c445dc5b52fd53cb6bb88800de08dbfcf0717f
sagomezmartinez <sagomezmartinez@deloitte.com>
2023-10-16 12:55:33 -0600

    Database Upgrade - First Commit

--- app/src/main/java/com/example/tasktimer/AppDatabase.kt
+++ app/src/main/java/com/example/tasktimer/AppDatabase.kt
@@ -9,7 +9,7 @@ import java.lang.IllegalStateException
 private const val TAG = "AppDatabase"
 
 private const val DATABASE_NAME = "TaskTimer.db"
-private const val DATABASE_VERSION = 3
+private const val DATABASE_VERSION = 2
 
 internal class AppDatabase constructor(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,null,
     DATABASE_VERSION) {
@@ -18,7 +18,7 @@ internal class AppDatabase constructor(context: Context): SQLiteOpenHelper(conte
         Log.d(TAG,"AppDatabase: initialising")
     }
 
-    override fun onCreate(db: SQLiteDatabase?) {
+    override fun onCreate(db: SQLiteDatabase) {
         Log.d(TAG,"onCreate: starts")
         val sSQL = """CREATE TABLE ${TasksContract.TABLE_NAME} (
             ${TasksContract.Columns.ID} INTEGER PRIMARY KEY NOT NULL,
@@ -27,7 +27,7 @@ internal class AppDatabase constructor(context: Context): SQLiteOpenHelper(conte
             ${TasksContract.Columns.TASK_SORT_ORDER} INTEGER);
         """.replaceIndent(" ")
         Log.d(TAG,sSQL)
-        db?.execSQL(sSQL)
+        db.execSQL(sSQL)
 
         val sSQLTiming = """CREATE TABLE ${TimingsContract.TABLE_NAME} (
             ${TimingsContract.Columns.ID} INTEGER PRIMARY KEY NOT NULL,
@@ -35,18 +35,42 @@ internal class AppDatabase constructor(context: Context): SQLiteOpenHelper(conte
             ${TimingsContract.Columns.TIMING_START_TIME} INTEGER,
             ${TimingsContract.Columns.TIMING_DURATION} INTEGER);
         """.replaceIndent(" ")
-        Log.d(TAG,sSQL)
-        db?.execSQL(sSQL)
+        Log.d(TAG,sSQLTiming)
+        db.execSQL(sSQLTiming)
+
+        addTimingsTable(db)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         Log.d(TAG,"onUpgrade: starts")
         when(oldVersion) {
             1 -> {
-//                upgrade logic from version
+                addTimingsTable(db)
             }
             else -> throw IllegalStateException("onUpgrade() with unknown newVersion: $newVersion")
         }
+    }
+
+    private fun addTimingsTable(db: SQLiteDatabase) {
+        val sSQLTiming = """CREATE TABLE ${TimingsContract.TABLE_NAME} (
+            ${TimingsContract.Columns.ID} INTEGER PRIMARY KEY NOT NULL,
+            ${TimingsContract.Columns.TIMING_TASK_ID} INTEGER NOT NULL,
+            ${TimingsContract.Columns.TIMING_START_TIME} INTEGER,
+            ${TimingsContract.Columns.TIMING_DURATION} INTEGER);
+        """.replaceIndent(" ")
+        Log.d(TAG,sSQLTiming)
+        db.execSQL(sSQLTiming)
+
+        val sSQLTrigger = """CREATE TRIGGER Remove_Task
+            AFTER DELETE ON ${TasksContract.TABLE_NAME}
+            FOR EACH ROW
+            BEGIN
+            DELETE FROM ${TimingsContract.TABLE_NAME}
+            WHERE ${TimingsContract.Columns.TIMING_TASK_ID} = OLD.${TasksContract.Columns.ID};
+            END;
+        """.replaceIndent()
+        Log.d(TAG,sSQLTrigger)
+        db.execSQL(sSQLTrigger)
     }
 
     companion object : SingletonHolder<AppDatabase,Context>(::AppDatabase)
